51c9d490271c9f24969636cf5a63015da685ec3b
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2020-09-29 22:25:36 +0200

    sonar fix

--- src/ui-host/Program.cs
+++ src/ui-host/Program.cs
@@ -1,21 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿// Copyright (c) 2020 Yoann MOUGNIBAS
+//
+// This file is part of MusicWorkflow.
+//
+// MusicWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MusicWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MusicWorkflow.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace Mougnibas.MusicWorkflow.UIHost
 {
-    public class Program
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// ASP.NET Core host for blazor web assembly.
+    /// </summary>
+    public static class Program
     {
+        /// <summary>
+        /// ASP.NET Core entry point.
+        /// </summary>
+        /// <param name="args">Not used.</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create and return the host builder.
+        /// </summary>
+        /// <param name="args">Application run arguments.</param>
+        /// <returns>The host builder.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
--- src/ui-host/Startup.cs
+++ src/ui-host/Startup.cs
@@ -1,14 +1,28 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Linq;
+// Copyright (c) 2020 Yoann MOUGNIBAS
+//
+// This file is part of MusicWorkflow.
+//
+// MusicWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MusicWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MusicWorkflow.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace Mougnibas.MusicWorkflow.UIHost
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
