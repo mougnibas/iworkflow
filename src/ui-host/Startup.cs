f6462baa5216744386075d63318b8695fc3aa83d
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2020-09-29 22:31:15 +0200

    sonar fix

--- src/ui-host/Startup.cs
+++ src/ui-host/Startup.cs
@@ -23,25 +23,42 @@ namespace Mougnibas.MusicWorkflow.UIHost
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
 
+    /// <summary>
+    /// ASP.NET Core startup class.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets the configuration to use.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        /// Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">The services to add.</param>
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // This method gets called by the runtime.
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The web host environment.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -52,6 +69,7 @@ namespace Mougnibas.MusicWorkflow.UIHost
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
