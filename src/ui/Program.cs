5fdfac38b663169c1dbacffb99cfaafffa07bdc8
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2020-09-29 22:19:19 +0200

    sonar fix

--- src/ui/Program.cs
+++ src/ui/Program.cs
@@ -1,23 +1,40 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Mougnibas.MusicWorkflow.UI;
+// Copyright (c) 2020 Yoann MOUGNIBAS
+//
+// This file is part of MusicWorkflow.
+//
+// MusicWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MusicWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MusicWorkflow.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace Mougnibas.MusicWorkflow.UI
 {
-    public class Program
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+    /// <summary>
+    /// Blazor web assembly entry point class.
+    /// </summary>
+    public static class Program
     {
+        /// <summary>
+        /// Blazor web assembly entry point method.
+        /// </summary>
+        /// <param name="args">Not used.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            await builder.Build().RunAsync();
+            await builder.Build().RunAsync().ConfigureAwait(true);
         }
     }
 }
