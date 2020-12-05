e27086cc5ad90261e7c6552ca00d6d524c2c920c
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2020-12-05 09:52:07 +0100

    grpc

---
+++ src/microservice/music-microservice/Services/GreeterService.cs
@@ -0,0 +1,26 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace music_microservice
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHellooo(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
